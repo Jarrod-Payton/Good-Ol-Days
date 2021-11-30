4b8269eca7ca1c087cd4a55afc0970d7bfaa3196
John Melo <joaolucasrmelo@gmail.com>
2021-11-29 17:28:25 -0700

    env

--- Good-Ol-Days/client/app/env.js
+++ Good-Ol-Days/client/app/env.js
@@ -1,5 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const useSockets = false
+export const domain = 'dev-lszte4un.us.auth0.com'
+export const clientId = 'UxyDEa3WSyRfuRMFsP3hSI4Fhbo5MeFN'
+export const audience = 'https://good-ol-days.com'
