894eea2341f3b85481fe36f67d809c899677c6ea
Lucas Payton <lucasjpayton101@gmail.com>
2021-11-30 10:05:57 -0700

    Stuff

---
+++ Good-Ol-Days.client/src/components/Challenge.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row">
+    <div class="col-12">
+      <div class="card">
+        <div class="card-body">Let's Make Some Memories!</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  setup() {
+
+  },
+}
+</script>
+<style scoped>
+</style>
--- Good-Ol-Days.client/src/components/Post.vue
+++ Good-Ol-Days.client/src/components/Post.vue
@@ -11,7 +11,7 @@
 <script>
 export default {
   props: { post: { type: Object, required: true } },
-  setup(props) {},
+  setup(props) { },
 };
 </script>
 <style scoped>
