f1d656ea7f26b8ec39840f6da5e2f1dd440ecdb1
Lucas Payton <lucasjpayton101@gmail.com>
2021-11-29 20:14:19 -0700

    Bare Basics

---
+++ Good-Ol-Days.client/src/components/Post.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card">
+    <div class="card-img">
+      <img :src="post.imgUrl" alt="post image" />
+    </div>
+    <div class="card-body">
+      {{ post.title }}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: { post: { type: Object, required: true } },
+  setup(props) {},
+};
+</script>
+<style scoped>
+</style>
