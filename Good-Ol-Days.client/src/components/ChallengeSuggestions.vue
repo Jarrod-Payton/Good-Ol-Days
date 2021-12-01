02066e07564bb8d888d49e1aa4ba864e49202dca
Lucas Payton <lucasjpayton101@gmail.com>
2021-12-01 14:29:34 -0700

    AppState Big Changes

---
+++ Good-Ol-Days.client/src/components/ChallengeSuggestions.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card">
+    <div class="card-title">
+      {{ suggestion.body }}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: { suggestion: { type: Object, required: true } },
+  setup(props) {
+    return {
+
+    }
+  },
+}
+</script>
+<style scoped>
+</style>
