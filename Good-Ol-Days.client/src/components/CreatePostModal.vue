a85b7fac288ea27ad369874a8600a7ea06e618fc
Lucas Payton <lucasjpayton101@gmail.com>
2021-12-01 10:26:36 -0700

    Updates

--- Good-Ol-Days.client/src/components/CreatePostModal.vue
+++ Good-Ol-Days.client/src/components/CreatePostModal.vue
@@ -2,19 +2,30 @@
   <div class="modal fade" id="createPostModal">
     <div class="modal-dialog">
       <div class="modal-header">
-        <div class="modal-title">
-          <h1>Add To Album</h1>
-        </div>
+        <h1 class="text-white modal-title">Add To Album</h1>
       </div>
+      <form @submit.prevent="createPost">
+        <div class="modal-body">
+          <div class="row">
+            <div class="col-12">
+              <h10></h10>
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   </div>
 </template>
 <script>
 import { computed } from "@vue/reactivity"
 import { AppState } from "../AppState"
+import Pop from "../utils/Pop"
 export default {
   setup() {
     return {
+      async createPost() {
+        Pop.toast('HEllo')
+      },
       activeAlbum: computed(() => AppState.activeAlbum)
     }
   },
