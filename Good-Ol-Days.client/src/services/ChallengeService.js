fd1772b0c8ec567b2174985f02af88f9a5936f6b
Lucas Payton <lucasjpayton101@gmail.com>
2021-12-03 10:59:16 -0700

    Challenges Change

--- Good-Ol-Days.client/src/services/ChallengeService.js
+++ Good-Ol-Days.client/src/services/ChallengeService.js
@@ -21,16 +21,27 @@ class ChallengeService {
     AppState.activeChallenge = activeChallenge.data
   }
   async getChallenges() {
-    AppState.activeChallenge = {}
     if (AppState.activeAlbum.hasChallenges === true) {
       const res = await api.get(`api/albums/${AppState.activeAlbum.id}/challenges`)
       logger.log('challenges', res.data)
       AppState.challenges = res.data
       if (AppState.challenges.length > 0) {
-        AppState.activeChallenge = AppState.challenges.find(c => c.isActive === true)
+        let found = AppState.challenges.find(c => c.isActive === true)
+        if (found == undefined) {
+          AppState.activeChallenge = {}
+        } else {
+          AppState.activeChallenge = found
+          const check = new Date(AppState.activeChallenge.createdAt).getTime() + 60000
+          const today = new Date().getTime()
+          if (check < today) {
+            await api.delete(`api/challenges/${AppState.activeChallenge.id}`)
+            AppState.activeChallenge = {}
+          }
+        }
       }
     }
   }
+  // 606590170
 }
 
 export const challengeService = new ChallengeService()
