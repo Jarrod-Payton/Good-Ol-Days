751775559b3a6ceb2b30893a24630c5f3eab0418
John Melo <joaolucasrmelo@gmail.com>
2021-12-04 19:18:28 -0700

    Post Service doc

--- Good-Ol-Days.client/src/services/PostService.js
+++ Good-Ol-Days.client/src/services/PostService.js
@@ -7,17 +7,20 @@ import { api } from "./AxiosService"
 
 class PostService {
   setActive(id) {
+    // Set post to Active Post
     const active = AppState.posts.find(p => p.id === id)
     AppState.activePost = active
     logger.log('ACTIVE', AppState.activePost)
   }
   async getPosts(albumId) {
+    // Get all posts for this album with album ID
     const res = await api.get(`api/albums/${albumId}/posts`)
     logger.log('Posts', res.data)
     AppState.posts = res.data
     AppState.doneSyncing = true
   }
   async createPost(body, hasChallenge) {
+    // Create a new post with post body and if it has challenge or not
     if (hasChallenge === 'true') {
       body.challengeId = AppState.activeChallenge.id
     }
@@ -26,6 +29,7 @@ class PostService {
     AppState.posts.push(res.data)
   }
   async deletePost() {
+    // Deletes a post with the active Post ID
     await api.delete(`api/posts/${AppState.activePost.id}`)
     AppState.posts = AppState.posts.filter(p => p.id !== AppState.activePost.id)
     AppState.activePost = {}
