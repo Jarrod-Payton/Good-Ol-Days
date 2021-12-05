b343ef4795d8bbfd39d3642af072271495a48d64
John Melo <joaolucasrmelo@gmail.com>
2021-12-04 19:16:10 -0700

    CollaboratorService doc

--- Good-Ol-Days.client/src/services/CollaboratorService.js
+++ Good-Ol-Days.client/src/services/CollaboratorService.js
@@ -3,10 +3,12 @@ import { Collaborator } from "../models/Collaborator"
 import { logger } from "../utils/Logger"
 import Pop from "../utils/Pop"
 import { api } from "./AxiosService"
+import { notificationService } from "./NotificationService"
 
 
 class CollaboratorService {
   async getAllMyCollabAlbums() {
+    // Get all the colaborations verified true for this account
     const res = await api.get('account/collaborators')
     logger.log('MY COLLAB ALBUMS', res.data)
     const collab = res.data.map(c => c = new Collaborator(c))
@@ -14,6 +16,7 @@ class CollaboratorService {
     logger.log('APPSTATE COLLABORATORS', AppState.collaborators)
   }
   async getCollabThisAlbum(albumId) {
+    // Get all the collaborator for this album, verify true or false with album ID
     const res = await api.get('api/albums/' + albumId + '/collaborators')
     const collab = res.data.map(c => c = new Collaborator(c))
     AppState.collabThisAlbum = collab
@@ -22,20 +25,28 @@ class CollaboratorService {
 
 
   async verify(id) {
+    // Change the verify to true with the collaboration ID
     const res = await api.put('api/collaborators/' + id)
     logger.log('VERIFY COLLAB', res.data)
   }
   async addColab(albumId) {
+    // Add someone as collaborator but verify is false with the album ID
     const res = await api.post('api/collaborators', { albumId: albumId })
     logger.log('ADD COLLAB', res.data)
   }
   async acceptColab(notification) {
+    // Get the album that this collab if from, with album ID from the notification
     const album = AppState.myAlbums.find( a => a.id === notification.albumId)
     logger.log('TEST JOHN 1', album)
+    // Get All collabs on this album with the album ID
     await this.getCollabThisAlbum(album.id)
+    // Get the person that matches the notification that you accepted to change verify to true
     const collabToAccept = AppState.collabThisAlbum.find( c => c.accountId === notification.notifier)
     logger.log('TEST JOHN 2',collabToAccept)
+    // Change the verify to true with the collaboration ID
     await this.verify(collabToAccept.id)
+    // Delete that notification with notification ID
+    await notificationService.deleteNotification(notification.id)
     Pop.toast('Accepted Collab')
   }
 }
