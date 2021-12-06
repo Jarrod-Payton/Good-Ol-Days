9fe54a426b470551733c20466e8c4e9a4544796b
jacob carpenter <jakecarpenter407@gmail.com>
2021-12-06 11:41:56 -0700

    sockets implemented

--- Good-Ol-Days.client/src/services/SocketService.js
+++ Good-Ol-Days.client/src/services/SocketService.js
@@ -1,3 +1,5 @@
+import { AppState } from '../AppState'
+import { logger } from '../utils/Logger'
 import Pop from '../utils/Pop'
 import { SocketHandler } from '../utils/SocketHandler'
 
@@ -6,6 +8,12 @@ class SocketService extends SocketHandler {
     super()
     this
       .on('error', this.onError)
+      .on('NEW_NOTIFICATION', this.recieveCollabNotification)
+    //TODO write recieved function here to catch emits from server
+  }
+  recieveCollabNotification(payload) {
+    logger.log('HIT_DA_SOCKET')
+    AppState.notifications = payload
   }
 
   onError(e) {
--- Good-Ol-Days.client/src/utils/FirebaseProvider.js
+++ Good-Ol-Days.client/src/utils/FirebaseProvider.js
@@ -3,6 +3,8 @@ import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
 import 'firebase/compat/storage'
 
+
+//Exporting all of the useful functions and initializing our firebase app
 export const fb = firebase.initializeApp(firebaseConfig)
 export const fbAuth = fb.auth()
 export const storage = fb.storage()
