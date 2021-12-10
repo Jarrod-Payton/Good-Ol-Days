a67dbb8c5891e77194697c7fa7a81a655a6ca3d6
John Melo <joaolucasrmelo@gmail.com>
2021-12-10 15:48:55 -0700

    transition

--- Good-Ol-Days.client/src/App.vue
+++ Good-Ol-Days.client/src/App.vue
@@ -63,36 +63,4 @@ p {
   background-color: #adadad;
   border-radius: 7px;
 }
-
-//route
-/* route transitions */
-.route-enter-from {
-  opacity: 1;
-  transform: translateX(100px);
-}
-.route-enter-active {
-  transition: all 0.3s ease-out;
-}
-.route-leave-to {
-  opacity: 1;
-  transform: translateX(-100px);
-}
-.route-leave-active {
-  transition: all 0.3s ease-in;
-}
-
-// .enter-from {
-//   opacity: 1;
-//   transform: translateX(100px);
-// }
-// .enter-active {
-//   transition: all 0.3s ease-out;
-// }
-// .leave-to {
-//   opacity: 1;
-//   transform: translateX(-100px);
-// }
-// .leave-active {
-//   transition: all 0.3s ease-in;
-// }
 </style>
