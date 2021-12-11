5b8697854c9ab27c9b9dc950e92721ccbc14e0af
Lucas Payton <lucasjpayton101@gmail.com>
2021-12-10 17:35:00 -0700

    Howdy

--- Good-Ol-Days/firebase-admin.js
+++ Good-Ol-Days/firebase-admin.js
@@ -2,7 +2,7 @@ export const firebaseAccountKey = {
   type: 'service_account',
   project_id: 'good-ol--days',
   private_key_id: process.env.PRIVATE_KEY_ID,
-  private_key: process.env.PRIVATE_KEY,
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
   client_email: 'firebase-adminsdk-6wz9t@good-ol--days.iam.gserviceaccount.com',
   client_id: '109635498095923900965',
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
